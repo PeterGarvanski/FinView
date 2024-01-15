ccc9593eed7f19eb99922de64707942dcfd37869
Peter Garvanski <garvanski.peter@gmail.com>
2024-01-15 14:11:22 +0000

    feat: completed the chart for assets

---
+++ financetracker/static/js/assets.js
@@ -0,0 +1,19 @@
+// Add Doughnut Chart data and options here// Add Doughnut Chart data and options here
+
+var assetCtx = document.getElementById('asset-chart').getContext('2d');
+var assetDoughnutChart = new Chart(assetCtx, {
+    type: 'doughnut',
+    data: {
+        labels: ["House", "Stocks", "Crypto", "Cars"],
+        datasets: [{
+            data: [45, 27, 61, 36],
+            borderColor: "#edf0f2",
+            backgroundColor: ["#2d2354", "#edf0f2a8"],
+            borderWidth: 4,
+        }]
+    },
+
+    options: {
+        display: false, // Set to false to hide the legend
+    }
+});
--- financetracker/static/js/dashboard.js
+++ financetracker/static/js/dashboard.js
@@ -1,11 +1,12 @@
 // Add Doughnut Chart data and options here
-var ctx = document.getElementById('doughnutChart').getContext('2d');
-var doughnutChart = new Chart(ctx, {
+
+var savingsCtx = document.getElementById('savings-chart').getContext('2d');
+var savingsDoughnutChart = new Chart(savingsCtx, {
     type: 'doughnut',
     data: {
         datasets: [{
             data: [33, 67, 100],
-            backgroundColor: ["#edf0f221", "#edf0f2a8", "#ffffff00"],
+            backgroundColor: ["#2d2354", "#edf0f2a8", "#ffffff00"],
             borderColor: ["#edf0f2", "#edf0f2", "#ffffff00"],
             borderWidth: 4,
         }]
@@ -15,14 +16,13 @@ var doughnutChart = new Chart(ctx, {
     }
 });
 
-
-var ctx = document.getElementById('doughnutChart2').getContext('2d');
-var doughnutChart = new Chart(ctx, {
+var netWorthCtx = document.getElementById('net-worth-chart').getContext('2d');
+var netWorthDoughnutChart = new Chart(netWorthCtx, {
     type: 'doughnut',
     data: {
         datasets: [{
             data: [7611, 2389, 10000],
-            backgroundColor: ["#edf0f221", "#edf0f2a8", "#ffffff00"],
+            backgroundColor: ["#2d2354", "#edf0f2a8", "#ffffff00"],
             borderColor: ["#edf0f2", "#edf0f2", "#ffffff00"],
             borderWidth: 4,
         }]
